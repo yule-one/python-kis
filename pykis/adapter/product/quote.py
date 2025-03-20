72e6a039f3a97efc2d40ca0d8fe3adab170eaf65
yule-one <yule_one@icloud.com>
2025-03-20 19:21:42 +0900

    Update quote.py

--- pykis/adapter/product/quote.py
+++ pykis/adapter/product/quote.py
@@ -94,7 +94,7 @@ class KisQuotableProduct(Protocol):
     def quote(
         self,
         extended: bool = False,
-        market: str = "J",
+        tv: str = "J",
     ) -> KisQuoteResponse:
         """
         한국투자증권 주식 현재가 조회
